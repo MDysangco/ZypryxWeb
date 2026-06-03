d9ead425e292e1e2fe07a7628d23cc5c395205f7
MDysangco <dysangcom@gmail.com>
2026-06-03 17:48:10 +0100

    First commit

---
+++ ZypryxWeb/tailwind.config.js
@@ -0,0 +1,12 @@
+﻿module.exports = {
+    content: [
+        "./Pages/**/*.{cshtml,razor}",
+        "./Views/**/*.{cshtml,razor}",
+        "./Shared/**/*.{cshtml,razor}",
+        "./wwwroot/**/*.html"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};

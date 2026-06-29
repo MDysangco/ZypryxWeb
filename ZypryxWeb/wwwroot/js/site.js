7a203aee0e8d52fb70b492b9ef60572b1d6367f5
MDysangco <dysangcom@gmail.com>
2026-06-29 22:29:57 +0100

    Quick save

--- ZypryxWeb/wwwroot/js/site.js
+++ ZypryxWeb/wwwroot/js/site.js
@@ -1,24 +1,20 @@
 ﻿
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
-    document.body.style.backgroundColor = "white";
 }
 
 
 function toggleNav() {
-
     var sideNav = document.getElementById("mySidenav");
-    if (sideNav.style.width <= 0) {
-        openNav()
+    var width = parseInt(sideNav.style.width) || 0;
+    if (width === 0) {
+        openNav();
     } else {
-        closeNav()
+        closeNav();
     }
 }
 
